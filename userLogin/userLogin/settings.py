9e96a5b94da4c9d85dff8fda4687f8c087c9e150
ongraphpythondev <pythondev@ongraph.com>
2019-09-23 10:31:47 +0530

    fixtures added

--- userLogin/userLogin/settings.py
+++ userLogin/userLogin/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+FIXTURE_DIRS = (
+   'verify_app/fixtures/initial_data.json',
+)
--- userLogin/verify_app/views.py
+++ userLogin/verify_app/views.py
@@ -4,15 +4,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import LoginForm, UserRegistrationForm
 
+
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-           #authenticate the registered user
-            user = authenticate(request,
-                                        username=cd['username'],
-                                        password=cd['password'])
+            # Authenticate the registered user
+            user = authenticate(request, username=cd['username'], password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
@@ -36,10 +35,11 @@ def register(request):
             new_user.set_password(user_form.cleaned_data['password'])
             # Save the User object
             new_user.save()
-            return render(request,'verify_app/register_done.html',{'new_user': new_user})
+            return render(request, 'verify_app/register_done.html', {'new_user': new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request,'verify_app/register.html',{'user_form': user_form})
+    return render(request, 'verify_app/register.html', {'user_form': user_form})
+
 
 def logout_request(request):
     logout(request)
