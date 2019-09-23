c9afb668d11d6481aba1fc21e244855f5a6a49b1
ongraphpythondev <pythondev@ongraph.com>
2019-09-23 10:32:21 +0530

    add is_active in list_display

--- userLogin/verify_app/admin.py
+++ userLogin/verify_app/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import ugettext_lazy as _
 from .models import User
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for  User model with username field."""
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'first_name', 'last_name', 'is_staff', 'is_active')
+    search_fields = ('username', 'first_name', 'last_name')
+    ordering = ('username',)
